2d126a11b5ce53959f78d249373944f3b3cacc88
Travis Clarkson <tk1667@gmail.com>
2018-02-19 03:08:43 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,6 @@ katz_deli = []
 
 def line(queue)
   string = ""
-  queue.each_with_index {|name, index| string+= index + ". " + name }
+  queue.each_with_index {|name, index| string= string + index + ". " + name }
   queue.length == 0 ? (puts "The line is currently empty.") : (puts "The line is currently: #{string}}")
 end
